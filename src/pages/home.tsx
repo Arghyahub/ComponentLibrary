8aeccfee8a7d42e08753c997993d567dfb70e63b
Arghya <arghyadas242004@gmail.com>
2024-04-06 23:21:39 +0530

    add: Link to alert modal

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,15 +2,23 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className="flex flex-col gap-2 h-screen w-screen p-5 bg-[#282828] text-white">
-      <h1 className="text-3xl font-bold mb-4">Component library</h1>
-      <ul className="list-disc list-inside">
+    <div className="flex flex-col gap-2 bg-[#282828] p-5 w-screen h-screen text-white">
+      <h1 className="mb-4 font-bold text-3xl">Component library</h1>
+      <ul className="flex flex-col gap-2 list-disc list-inside">
         <li>
           <Link
             to={'/accordion'}
-            className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+            className="text-blue-600 hover:text-blue-800 visited:text-purple-600 underline"
           >
             Accordion
+          </Link>
+        </li>
+        <li>
+          <Link
+            to={'/alertModal'}
+            className="text-blue-600 hover:text-blue-800 visited:text-purple-600 underline"
+          >
+            Alert Modal
           </Link>
         </li>
       </ul>
